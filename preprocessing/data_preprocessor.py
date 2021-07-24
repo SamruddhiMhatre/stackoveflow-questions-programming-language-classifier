ee282025585b337084786a5eb5eafd0a03d23182
Samruddhi Mhatre <35568225+SamruddhiMhatre@users.noreply.github.com>
2021-07-24 17:19:22 +0530

    Update data_preprocessor.py

--- preprocessing/data_preprocessor.py
+++ preprocessing/data_preprocessor.py
@@ -21,3 +21,37 @@ class DataPreprocess:
       labels.append(label_batch)
 
     return texts, labels
+
+  
+  def text_standardization(self, text):
+    """
+    Input: Batch Text list
+
+    Output: Returns list after removing the html tags from the lower cased text
+    """
+
+    self.text = text
+    text = tf.strings.lower(text)
+    cleantext = tf.strings.regex_replace(text,"<[^>]+>", " ")
+
+    return cleantext
+
+
+  
+  def vectorize_text(self, max_features,text, label):
+
+    self.max_features = max_features
+    self.text = text
+    self.label = label
+  
+    vectorize_layer = TextVectorization(   # for batch of strings
+    standardize=self.text_standardization,
+    max_tokens=max_features,
+    output_mode='binary'              #  binary mode to build bag of words model
+    )
+
+    vectorize_layer.adapt(self.text)
+
+    text = tf.expand_dims(self.text, -1)
+    
+    return vectorize_layer(text), self.label
